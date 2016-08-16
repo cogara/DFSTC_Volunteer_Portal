ecb8139d268cac22160345b3d97f6b225a3e42db
Connor O'Gara <ogara.connor@gmail.com>
2016-08-16 15:29:45 -0500

    added start function to npm

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const app = express();
 const index = require('./routes/index.js');
 
 
-
 app.use(express.static('public'));
-app.use('/', index);
 
+app.use('/', index);
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, function() {
