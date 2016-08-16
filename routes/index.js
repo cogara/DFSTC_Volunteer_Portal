6d257be2cd222047f3341f08473a2596aca11a00
Connor O'Gara <ogara.connor@gmail.com>
2016-08-16 15:25:06 -0500

    setup skeleton

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const path = require('path');
+
+router.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, '..', 'public', 'views', 'index.html'))
+})
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const index = require('./routes/index.js');
+
+
+
+app.use(express.static('public'));
+app.use('/', index);
+
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, function() {
+  console.log('listening on port:', port);
+})
