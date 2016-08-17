ce596f98a9ffb31f89d258b3bfa91ad25f357027
tracyvincent <tracykvincent@gmail.com>
2016-08-17 17:07:04 -0500

    adds basic appointment schema

---
+++ models/appointment.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var appointmentSchema = new Schema(
+  {
+    title: {type: String, default: 'Image Coach Appointment'},
+    date: Date,
+    startTime: Date,
+    endTime: Date,
+    volunteerSlots: Number,
+    clientSlots: Object,
+
+    // this object will have a value for each client to indicate appointment type - e.g.
+    // {
+    // appointmentType: interview,
+    // appointmentType: wardrobe,
+    // appointmentType: wardrobe
+    // }
+
+    trainingAppointment: Boolean
+  }
+);
+
+
+module.exports = mongoose.model('Appointment', appointmentSchema);
