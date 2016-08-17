6db026ac72a96eb8bfee9eee0a3b6d500b8861fc
Connor O'Gara <ogara.connor@gmail.com>
2016-08-17 11:16:26 -0500

    npm start updated, more dependencies

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ const express = require('express');
 const jsonwebtoken = require('jsonwebtoken');
 const expressJwt = require('express-jwt');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const index = require('./routes/index.js');
 const register = require('./routes/register.js');
 
 const app = express();
 
+mongoose.connect(config.database);
+
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.use('/', index);
 app.use('/register', register);
