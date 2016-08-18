21ba10bd18b2badd060c909a194001357ea5c822
Megan Martinson <hello@meg.codes>
2016-08-18 11:39:10 -0500

    Finished registration?

--- public/scripts/registerController.js
+++ public/scripts/registerController.js
@@ -1,33 +1,94 @@
 angular.module('DfstcSchedulingApp').controller('registerController', function(UserService){
 var vm = this;
-var user = {
-};
+// USER SETUP
+var user = {};
 user.email = vm.email;
 user.password = vm.password;
 user.firstName = vm.firstName;
 user.lastName = vm.lastName;
 user.phoneNumber = vm.phoneNumber;
-user.address = 
-user.addressOne = vm.addressOne;
-user.addressTwo = vm.addressTwo;
-user.city = vm.city;
-user.state = vm.state;
-user.zipcode = vm.zipcode;
+user.address = {address1: vm.addressOne,
+address2: vm.addressTwo,
+city: vm.city,
+state: vm.state
+zip: vm.zipcode};
+user.potentialPos = vm.volunteerOpportunities;
+// PUSHING NAMES OF OPPORTUNITIES INTO AN ARRAY
+vm.volunteerOpportunities = [];
+if(vm.imageCoach == true){
+  volunteerOpportunities.push("Image Coach")
+};
+if(vm.clothingSorter == true){
+  volunteerOpportunities.push("Clothing Sorter")
+};
+if(vm.clothingOrganizer == true){
+  volunteerOpportunities.push("Clothing Organizer")
+};
+if(vm.frontDesk == true){
+  volunteerOpportunities.push("Front Desk")
+};
+if(vm.clothingSale == true){
+  volunteerOpportunities.push("Clothing Sale Volunteer")
+};
+if(vm.clothingSaleCaptain == true){
+  volunteerOpportunities.push("Clothing Sale Captain")
+};
+if(vm.careerAdvocate == true){
+  volunteerOpportunities.push("Career Advocate")
+};
+if(vm.intelligenceMentor == true){
+  volunteerOpportunities.push("Intelligence Mentor")
+};
+if(vm.intelligenceMuse == true){
+  volunteerOpportunities.push("Intelligence Muse")
+};
+if(vm.communityAmbassador == true){
+  volunteerOpportunities.push("Community Ambassador")
+};
+if(vm.eventSetUp == true){
+  volunteerOpportunities.push("Event Set Up")
+};
+if(vm.photographer == true){
+  volunteerOpportunities.push("Photographer")
+};
+if(vm.graphicDesigner == true){
+  volunteerOpportunities.push("Graphic Designer")
+};
+// END POSITIONS
 
-user.imageCoach = vm.imageCoach;
-user.clothingSorter = vm.clothingSorter;
-user.clothingOrganizer = vm.clothingOrganizer;
-user.frontDesk = vm.frontDesk;
-user.clothingSale = vm.clothingSale;
-user.clothingSaleCaptain = vm.clothingSaleCaptain;
-user.careerAdvocate = vm.careerAdvocate;
-user.intelligenceMentor = vm.intelligenceMentor
-user.intelligenceMuse = vm.intelligenceMuse;
-user.communityAmbassador = vm.communityAmbassador;
-user.eventSetUp = vm.eventSetUp;
-user.photographer = vm.photographer;
-user.graphicDesigner = vm.graphicDesigner;
-
-
+user.company = vm.company;
+user.organization = vm.group;
+user.isAvail = {
+  Monday: {
+    Morning: vm.mondayMorning,
+    Afternoon: vm.mondayAfternoon,
+    Evening: vm.mondayEvening
+  },
+  Tuesday: {
+    Morning: vm.tuesdayMorning,
+    Afternoon: vm.tuesdayAfternoon,
+    Evening: vm.tuesdayEvening
+  },
+  Wednesday: {
+    Morning: vm.wednesdayMorning,
+    Afternoon: vm.wednesdayAfternoon,
+    Evening: vm.wednesdayEvening
+  },
+  Thursday: {
+    Morning: vm.thursdayMorning,
+    Afternoon: vm.thursdayAfternoon,
+    Evening: vm.thursdayEvening
+  },
+  Friday: {
+    Morning: vm.fridayMorning,
+    Afternoon: vm.fridayAfternoon,
+    Evening: vm.fridayEvening
+  },
+  Saturday: {
+    Morning: vm.saturdayMorning,
+    Afternoon: vm.saturdayAfternoon,
+    Evening: vm.saturdayEvening
+  }
+};
 
 });
