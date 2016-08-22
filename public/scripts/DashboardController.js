2b4bddac60a00720e91b5003c83f2de6816f1d70
tracyvincent <tracykvincent@gmail.com>
2016-08-21 22:18:22 -0500

    appointment form posting to the database

--- public/scripts/DashboardController.js
+++ public/scripts/DashboardController.js
@@ -1,7 +1,39 @@
 angular.module('DfstcSchedulingApp').controller('DashboardController', DashboardController);
 
-function DashboardController($http, $state, UserService) {
+function DashboardController($http, $state, UserService, AppointmentService) {
   var vm = this;
+
+  vm.openProfile = openProfile;
+
+  console.log('hello?');
+  function openProfile(id) {
+    console.log('sending id', id);
+
+    var modalInstance = $modal.open({
+      animation: true,
+      templateUrl: 'myModalContent.html',
+      controller: 'ProfileController',
+      controllerAs: 'prof',
+      size: 'lg',
+      resolve: {
+        profile: function (UserService) {
+          return UserService.getProfile(id).then(function(response){
+            return response;
+          });
+        }
+      }
+    });
+
+    modalInstance.result.then(function (profile) {
+      //do function to save new profile info
+      return UserService.editProfile(profile).then(function() {
+        console.log('promise?');
+        window.location.reload();
+      });
+
+      console.log(profile);
+    });
+  };
 
       //These variables MUST be set as a minimum for the calendar to work
       vm.calendarView = 'month';
@@ -20,6 +52,11 @@ function DashboardController($http, $state, UserService) {
 
       vm.addAppointment = function(){
       console.log(vm.appointment);
+      AppointmentService.addAppointment(vm.appointment).then(function(response){
+        console.log('add appointment success', response.data);
+      }, function(response){
+        console.log('add appointment fail', response.data);
+      })
       }
 
       vm.appointment={
