27a1f944227880d7581c1f319b95419b51af2b6c
forente <zerofox16@gmail.com>
2016-08-26 08:45:38 -0500

    added announcemnets to mke it work

---
+++ models/announcement.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var announcementSchema = new Schema ({
+  title:String,
+  date: Date,
+  massage:String
+});
+
+module.exports = mongoose.model('announcement', announcementSchema);
