eb31f00038f9c9d06ca2433a11aec2f662c3134c
Connor O'Gara <ogara.connor@gmail.com>
2016-08-29 12:05:48 -0500

    upload fixed

--- routes/register.js
+++ routes/register.js
@@ -2,6 +2,22 @@ const router = require('express').Router();
 const path = require('path');
 const User = require('../models/user.js');
 
+var storage  = multer.diskStorage({
+  destination: function(request, file, cb){
+    cb(null, './userImages')
+  },
+  filename: function(req, file, cb) {
+    console.log('Date Now', Date.now());
+    console.log('Date Now', Date.now());
+    console.log('Date Now', Date.now());
+    cb(null, Date.now().toString() + '.jpg')
+  }
+});
+
+var upload = multer({
+  storage:storage
+}).single('photo');
+
 router.post('/', function(request, response) {
   var info = request.body;
   User.findOne({email: info.email}, function(err, exists) {
