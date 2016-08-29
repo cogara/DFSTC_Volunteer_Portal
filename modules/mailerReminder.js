5b87c625d1c1650f600e8cf1c0480a7fd4440261
forente <zerofox16@gmail.com>
2016-08-29 16:27:34 -0500

    Formatted date and time

--- modules/mailerReminder.js
+++ modules/mailerReminder.js
@@ -1,53 +1,11 @@
 var router = require('express').Router();
 var nodemailer = require('nodemailer');
+const moment = require('moment');
 
 var user = require('../models/user');
 var appointment = require('../models/appointment');
 
-var messageBlob = '<p>' +
-'Dear ~~~First Name~~~;' +
-'</p>' +
-'<p>' +
-'  Thank you for volunteering for an Image Coaching shift this week.' +
-'</p>' +
-'<p>' +
-'  Below are the details for your appointment:' +
-'</p>' +
-'<p>' +
-'  Date: <MM-DD-YYYY>' +
-'</p>' +
-'<p>' +
-'  Start time:<00:00 AM CDT>' +
-'</p>' +
-'<p>' +
-'  Please arrive 10-15 minutes prior to your appointment time. Put your best' +
-'  foot forward and be sure to dress professional and in comfortable foot' +
-'  wear.' +
-'</p>' +
-'<p>' +
-'  We are located at 1549 University Avenue W., St. Paul, MN 5104' +
-'</p>' +
-'<p>' +
-'  Our parking lot is located directly behind the building. You can park in' +
-'  any of the spots labeled "Dress for Success" or "Ashton Building" and enter' +
-'  our offices through the orange door furthest to the left, labeled Dress for' +
-'  Success.' +
-'</p>' +
-'<p>' +
-'  If you are running late, lost or unable to make your appointment due to an' +
-'  emergency please call us at 651-646-6000.' +
-'</p>' +
-'<p>' +
-'  Thank you for being part of our mission to empower low-income women to' +
-'  achieve economic independence. You are helping to make a difference in Twin' +
-'  Cities women\'s lives. Since opening our doors in 2010, Dress for Success' +
-'  Twin Cities has worked with over 3,500 women.' +
-'</p>' +
-'<p>' +
-'  To sign for more Volunteer Opportunities Click here or visit our website' +
-'  for more information.' +
-'  <link>' +
-'</p>';
+
 
 
 var mailReminder = function(){
@@ -67,7 +25,7 @@ var mailReminder = function(){
       console.log(theAppointment);
       theAppointment.forEach(function(element){
         //console.log(element.startTime +" Awesome!" );
-        //TODO: fix up mai;l sender
+        //TODO: fix up mail sender
         mailToUser(element);
       });
     }
@@ -102,10 +60,51 @@ function mailToUser (apmt){
        subject: 'Image Coaching Reminder ✔', //
 
        // HTML body
-       html: '<p><b>Hello</b> to myself </p>' +
-           '<p>Hello '+volunteer.firstName+'</p> New message sent: ' +new Date()+
-           '<p>Appointment time at '+apmt.startTime+' </p>'
-           +messageBlob
+       html:'<p>' +
+       'Dear '+volunteer.firstName+';' +
+       '</p>' +
+       '<p>' +
+       '  Thank you for volunteering for an Image Coaching shift this week.' +
+       '</p>' +
+       '<p>' +
+       '  Below are the details for your appointment:' +
+       '</p>' +
+       '<p>' +
+       '  Date: '+moment(apmt.startsAt).format("dddd, MMMM Do YYYY")+//(apmt.startsAt.getMonth()+1)+'/'+apmt.startsAt.getUTCDate()+'/'+apmt.startsAt.getUTCFullYear()+
+       '</p>' +
+       '<p>' +
+       '  Start time: '+moment(apmt.startsAt).format('h:mm a')+//apmt.startsAt)+
+       '</p>' +
+       '<p>' +
+       '  Please arrive 10-15 minutes prior to your appointment time. Put your best' +
+       '  foot forward and be sure to dress professional and in comfortable foot' +
+       '  wear.' +
+       '</p>' +
+       '<p>' +
+       '  We are located at 1549 University Avenue W., St. Paul, MN 5104' +
+       '</p>' +
+       '<p>' +
+       '  Our parking lot is located directly behind the building. You can park in' +
+       '  any of the spots labeled "Dress for Success" or "Ashton Building" and enter' +
+       '  our offices through the orange door furthest to the left, labeled Dress for' +
+       '  Success.' +
+       '</p>' +
+       '<p>' +
+       '  If you are running late, lost or unable to make your appointment due to an' +
+       '  emergency please call us at 651-646-6000.' +
+       '</p>' +
+       '<p>' +
+       '  Thank you for being part of our mission to empower low-income women to' +
+       '  achieve economic independence. You are helping to make a difference in Twin' +
+       '  Cities women\'s lives. Since opening our doors in 2010, Dress for Success' +
+       '  Twin Cities has worked with over 3,500 women.' +
+       '</p>' +
+       '<p>' +
+       '  To sign for more Volunteer Opportunities Click here or visit our website' +
+       '  for more information.' +
+       '  <link>' +
+       '</p>'
+
 
        };
 
