2c7f4d18329eb945c204b1c7617f3e0484bc9ff3
forente <zerofox16@gmail.com>
2016-08-31 11:41:02 -0500

    added mailer temmplate

--- routes/cancel.js
+++ routes/cancel.js
@@ -5,6 +5,54 @@ var router =  require('express').Router();
 
 
 
+function mailToUser (apmt){
 
+    var volunteer = "default";
+    user.find( function(err, theVolenteer){
+       volunteer = theVolenteer[0]
+
+       var transporter = nodemailer.createTransport({
+         service:'Gmail',
+         //TODO: Change to an actual secret account and if gmail sett to less secure in security settings
+         auth:{
+           user:'dfstc016@gmail.com',
+           pass:'dressForSuccess'
+         },
+         logger:false,
+         debug:false
+       },{
+         // default message
+         from: 'Dress for Success Twin Cities <dfstc016@gmail.com>',
+       });
+
+       var message = {
+         // Comma separated list of recipients
+       to: '"'+volunteer.firstName +'" <zerofox16@gmail.com>',
+
+       // Subject of the message
+       subject: 'Image Coaching Reminder ✔', //
+
+       // HTML body
+       html:"THEY Canceled Panic!"
+
+
+       };
+
+       transporter.sendMail(message, function(err, info){
+         if (err) {
+           console.log(err);
+           return;
+         }
+
+       //console.log('Message sent successfully!');
+       //console.log('Server responded with "%s"', info.response);
+
+
+      });
+
+    //console.log(name.select('firstName'));
+
+    });
+}
 
 module.exports = router;
