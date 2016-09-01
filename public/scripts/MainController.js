de3618a965cf9636c7ce174cd7f96a447afe086a
Connor O'Gara <ogara.connor@gmail.com>
2016-09-01 15:57:42 -0500

    removed duplicate function

--- public/scripts/MainController.js
+++ public/scripts/MainController.js
@@ -83,15 +83,8 @@ function MainController($http, $state, $window, $scope, $uibModal, UserService, 
       UserService.currentUser.user = vm.currentUser;
       $state.go('dashboard');
     }, function(){
-        vm.currentUser = null;
-      });
-    }
-
-  function logout() {
-    UserService.logout().then(function(response) {
       vm.currentUser = null;
-      $state.go('/');
-    })
+    });
   }
 
   //checks if user is currently logged in on page load
@@ -100,5 +93,51 @@ function MainController($http, $state, $window, $scope, $uibModal, UserService, 
     vm.currentUser = response;
     UserService.currentUser.user = vm.currentUser;
   });
+
+
+  //QOL Demo Purposes;
+  vm.registerUser = {};
+  vm.registerUser.address = {};
+  vm.registerUser.address.addressOne = "3552 Main St";
+  vm.registerUser.address.city = "St. Paul";
+  vm.registerUser.address.state = "MN";
+  vm.registerUser.address.zip = "55109";
+  vm.registerUser.address.company = "Wells Fargo";
+  vm.registerUser.email = "marie.foster@gmail.com";
+  vm.registerUser.firstName = "Marie";
+  vm.registerUser.isAvail = {};
+  vm.registerUser.isAvail.monday = {};
+  vm.registerUser.isAvail.tuesday = {};
+  vm.registerUser.isAvail.wednesday = {};
+  vm.registerUser.isAvail.thursday = {};
+  vm.registerUser.isAvail.friday = {};
+  vm.registerUser.isAvail.saturday = {};
+  vm.registerUser.isAvail.monday.morning = true;
+  vm.registerUser.isAvail.monday.afternoon = false;
+  vm.registerUser.isAvail.monday.evening = false;
+  vm.registerUser.isAvail.tuesday.morning = true;
+  vm.registerUser.isAvail.tuesday.afternoon = true;
+  vm.registerUser.isAvail.tuesday.evening = true;
+  vm.registerUser.isAvail.wednesday.morning = false;
+  vm.registerUser.isAvail.wednesday.afternoon = false;
+  vm.registerUser.isAvail.wednesday.evening = true;
+  vm.registerUser.isAvail.thursday.morning = true;
+  vm.registerUser.isAvail.thursday.afternoon = true;
+  vm.registerUser.isAvail.thursday.evening = true;
+  vm.registerUser.isAvail.friday.morning = true;
+  vm.registerUser.isAvail.friday.afternoon = false;
+  vm.registerUser.isAvail.friday.evening = false;
+  vm.registerUser.isAvail.saturday.morning = false;
+  vm.registerUser.isAvail.saturday.afternoon = false;
+  vm.registerUser.isAvail.saturday.evening = false;
+  vm.registerUser.isTrainee = true;
+  vm.registerUser.jobTitle = "Bank Teller";
+  vm.registerUser.company = "Wells Fargo";
+  vm.registerUser.lastName = "Foster";
+  vm.registerUser.password = "marie";
+  vm.registerUser.phoneNumber = "5555342234";
+  vm.registerUser.volunteerOpportunities = {};
+  vm.registerUser.volunteerOpportunities.careerAdvocate = true;
+  vm.registerUser.volunteerOpportunities.imageCoach = true;
 
 } //end Main Controller
