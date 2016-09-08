3706695d0ef6f45d621ef663c6db7204bd35ac42
Connor O'Gara <ogara.connor@gmail.com>
2016-09-08 12:11:53 -0500

    removed console logs

--- modules/LastAppointmentScheduler.js
+++ modules/LastAppointmentScheduler.js
@@ -11,23 +11,16 @@ function lastAppointment() {
   var today  = new Date(new Date().setDate(new Date().getDate()));
   yesterday.setHours(0,0,0,0);
   today.setHours(0,0,0,0);
-  console.log(yesterday);
-  console.log(today);
-  console.log(new Date());
   var volunteers = [];
 
 
-    // queries the server for apponitments between tomorrow and the next day for reminders
+    // queries the server for apponintments between tomorrow and the next day for reminders
     Appointment.find({ "startsAt":{ $gte:  yesterday, $lt: today} }, function(err,appointments){
       if(err){
         console.log(err);
       }
-      console.log('appointments', appointments);
       for (var i = 0; i < appointments.length; i++) {
-        console.log('single appointment', appointments[i]);
-        // console.log('volunteers', appointments[i].volunteers);
         for (var j = 0; j < appointments[i].volunteers.length; j++) {
-          console.log('volunteer', appointments[i].volunteers[j]);
           var starts = moment(appointments[i].startsAt);
           var ends = moment(appointments[i].endsAt);
           var duration = moment.duration(ends.diff(starts));
